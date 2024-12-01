a6fd3ba1d41377cb491d01f39ac14a6cf2035933
Examen33 <sereja.buschuev@gmail.com>
2024-12-01 11:56:28 +0300

    Update generic_functions.py

--- datacenter/generic_functions.py
+++ datacenter/generic_functions.py
@@ -10,8 +10,10 @@ def get_duration(visit):
 
 
 def format_duration(duration):
-    return datetime.timedelta(seconds=int(duration.total_seconds()))
+    format_time = datetime.timedelta(seconds=int(duration.total_seconds()))
+    return format_time
 
 
 def is_visit_long(duration, minutes=60):
-    return int(duration.total_seconds() / 60) > minutes
+    sec_in_min = 60
+    return int(duration.total_seconds() / sec_in_min) > minutes
